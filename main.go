53e63ee652288fdd54c95bcc397539ff699f314a
mahigadamsetty <80254540+mahigadamsetty@users.noreply.github.com>
2022-03-18 22:27:12 +0530

    added a git-ignore

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 )
 
@@ -12,14 +11,9 @@ func main() {
 
 	flag.StringVar(&favMovie, "f", "", "favourite movies to be added")
 	flag.StringVar(&wishMovie, "w", "", "wishlist movies to be added")
-	wantWishList := flag.Bool("lw", false, "want wish list")
-	iBool := flag.Bool("i", false, "help flag")
-	flag.Parse()
 
-	if *iBool {
-		fmt.Println("f : favourite movies to be added")
-		return
-	}
+	wantWishList := flag.Bool("lw", false, "want wish list")
+	flag.Parse()
 
 	if wishMovie != "" {
 		if err := WriteToFile("wish_list.txt", wishMovie); err != nil {
